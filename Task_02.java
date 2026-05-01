fcd5a8b4b322e42658b4ba57ac8b782e9978b091
ayousha2025 <K253069@nu.edu.pk>
2026-05-01 22:29:38 +0500

    Task_02.java

--- Task_02.java
+++ Task_02.java
@@ -1,3 +1,13 @@
+// 1. Create a text file for a student. You can use the student’s name as the name of the file.
+// 2. Write the student’s full name and student ID in the file.
+// 3. Then create an arraylist that represents the courses that the student has registered for the semester.
+// Before registering for OOP Lab and OOP Theory check if the student has cleared both PF Lab
+// and Theory. If the student has obtained 50 or above 50 in PF Lab and Theory then add OOP Lab
+// and OOP theory in the arraylist.
+// 4. Each course is assumed to be of 3 credit hours so student can register for more than 15 hours in
+// the semester. Be careful while adding the courses in the list.
+// 5. Write all this information to the file. Read from the file and then display all the content.
+
 package FileHandling;
 
 import java.io.*;
